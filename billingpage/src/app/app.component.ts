a66801042b1507b8be8ecbdaaeed819aa68d9b8d
stephenjesus <stephenprakash5@gmail.com>
2017-08-06 14:39:59 +0530

    changes in billing

---
+++ billingpage/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import {Http,Response,HttpModule} from '@angular/http';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+   
+welcome='<--------PRODUCT_ENTRY-------->';
+ clickMessage = '';
+
+        products : [{
+        product_name: string,
+        product_code : string,
+        product_price:string;
+        product_gst:string;
+    }];
+  onClickMe() {
+    this.clickMessage ='5';
+  }
+
+  onSearchMe(){
+    this.products = null;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET','http://localhost:2500/searchdb',false)
+    xhttp.send();
+    this.products = JSON.parse( xhttp.responseText);
+    console.log(xhttp.responseText);
+  }
+
+
+      constructor()
+      {
+        this.welcome = "<--------PRODUCT_ENTRY-------->"
+
+       this.products = null;
+          var xhttp = new XMLHttpRequest();
+          xhttp.open('GET', 'http://localhost:2500/fetchallproductdetails', false);
+          xhttp.send();
+          this.products = JSON.parse(xhttp.responseText);
+          console.log(xhttp.responseText);
+        };       
+        
+
+} 
+ 
+ 
+ 
+ 
